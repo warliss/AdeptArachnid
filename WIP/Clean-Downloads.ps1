d63427f0a155058985673716b4bd490bacfec6b5
Wayne Arliss <7531439+warliss@users.noreply.github.com>
2021-01-30 10:14:27 +0000

    Clean-Downloads.ps1 file Initialized

---
+++ WIP/Clean-Downloads.ps1
@@ -0,0 +1,11 @@
+#### Clean-Downloads
+#
+# 2020-01-30    WA  Script created
+#
+### Description:
+#
+#  Scan through the users Downloads folder and moves files to locations based upon file type.
+#
+### File types:
+#
+# .iso, .zip, .exe, pdf
